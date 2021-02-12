bb1e48a0f9f13d01964989638ca69a2d58f94b32
Dark Man <77631739+dark-man99@users.noreply.github.com>
2021-02-12 18:40:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ if (message.content.startsWith("https://discord.gg/")) {
   message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/EXCuvNd`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
     
   client.channels.get("755636700482633791").send(
-`> <:ARBELE_UPUP:791411050238836766> By: <@${message.author.id}> ✅ 
+`> <> By: <@${message.author.id}> ✅ 
 ${message.content}`)
 }
     }
